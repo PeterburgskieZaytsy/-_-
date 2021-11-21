d9a79f4cfdd3e6a6d55fe73ff8565e89407f495a
PeterburgskieZaytsy <94259277+PeterburgskieZaytsy@users.noreply.github.com>
2021-11-21 08:22:23 +0300

    Add files via upload

--- prediction/main.py
+++ prediction/main.py
@@ -3,7 +3,14 @@ from utils import *
 
 
 def get_result(text):
-    predicted_category = get_bert_prediction(text)
+    
+    path_to_pretrained_model = '../model6'
+    model = BertForSequenceClassification.from_pretrained(path_to_pretrained_model)
+    tokenizer = BertTokenizerFast.from_pretrained('sberbank-ai/ruBert-base')
+    label_df = pd.read_csv('../label_df.csv')
+    
+    predicted_category = bert_inference(text, tokenizer, model, label_df)
+    
     return predicted_category
 
 
